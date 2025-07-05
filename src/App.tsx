caafa8f861b2698fceaf48359f256066212ed1b3
harukee-dev <hdjemsmc@gmail.com>
2025-07-05 19:07:52 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { MainPage } from './Pages/MainPage/MainPage'
 import { Header } from './Components/Header/Header'
 import { ErrorPage } from './Pages/ErrorPage/ErrorPage'
 
