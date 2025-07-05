9b0f33a62d0bc88e5e89716224f45989a945acb5
harukee-dev <hdjemsmc@gmail.com>
2025-07-05 14:08:04 +0300

    header done

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,16 +5,20 @@ import cl from './header.module.css'
 export const Header = () => {
   return (
     <div className={cl.headerContainer}>
-      <img
-        className={cl.pizzaLogo}
-        draggable={false}
-        src={pizzaLogo}
-        alt="logo"
-      />
-      <p className={cl.pizzaTitle}>Pizzateria</p>
-      <button className={cl.headerLink}>Why Pizzateria?</button>
-      <button className={cl.headerLink}>Menu</button>
-      <button className={cl.headerLink}>Contact</button>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          className={cl.pizzaLogo}
+          draggable={false}
+          src={pizzaLogo}
+          alt="logo"
+        />
+        <p className={cl.pizzaTitle}>Pizzateria</p>
+      </div>
+      <div className={cl.interactionContainer}>
+        <button className={cl.headerLink}>Why Pizzateria?</button>
+        <button className={cl.headerLink}>Menu</button>
+        <button className={cl.headerLink}>Contact</button>
+      </div>
       <img
         className={cl.userIcon}
         draggable={false}
