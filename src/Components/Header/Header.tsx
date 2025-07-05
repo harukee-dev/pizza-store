ef826ccc2b1633e5c361881de3714106e9266b13
harukee-dev <hdjemsmc@gmail.com>
2025-07-05 12:54:32 +0300

    added default header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,26 @@
+import pizzaLogo from './images/pizzateria-logo.png'
+import userIcon from './images/user-icon.png'
+import cl from './header.module.css'
+
 export const Header = () => {
   return (
-    <div>
-      <p>header</p>
+    <div className={cl.headerContainer}>
+      <img
+        className={cl.pizzaLogo}
+        draggable={false}
+        src={pizzaLogo}
+        alt="logo"
+      />
+      <p className={cl.pizzaTitle}>Pizzateria</p>
+      <button className={cl.headerLink}>Why Pizzateria?</button>
+      <button className={cl.headerLink}>Menu</button>
+      <button className={cl.headerLink}>Contact</button>
+      <img
+        className={cl.userIcon}
+        draggable={false}
+        src={userIcon}
+        alt="user-icon"
+      />
     </div>
   )
 }
