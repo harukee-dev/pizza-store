944f54890ace5a570a3ce4a69d72e76fbb16469d
RogeYy <egorbazenov2008@gmail.com>
2025-07-05 12:27:35 +0300

    added default server

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,8 +8,7 @@ app.use(express.json());
 app.get('/', (request, response) => {
 	response.send('Main Page')
 })
+
 app.listen(PORT, () => {
 	console.log('http://localhost:5000')
 })
-
-console.log("I XYESOS");
