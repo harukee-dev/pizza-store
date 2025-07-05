430690acd66cc6c28ce8de18c41c9cca17dba4eb
RogeYy <egorbazenov2008@gmail.com>
2025-07-05 18:21:36 +0300

    сервер и подключеный монгоБД

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
-const app = express()
-const PORT = 5000;
+const mongoose = require('mongoose');
 
+
+const app = express();
+const PORT = 5000;
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-	response.send('Main Page')
-})
+
+const DB_URL = 'mongodb+srv://Htabos:14563322asdZ@pizzadb.8u4bd1x.mongodb.net/?retryWrites=true&w=majority&appName=PizzaDB'; 
+
+async function connectDB() {
+  try {
+    await mongoose.connect(DB_URL);
+    console.log('MongoDB подключена успешно!');
+  } catch (err) {
+    console.error('Ошибка подключения к MongoDB:', err.message);
+    process.exit(1); // Завершаем процесс при ошибке
+  }
+}
+
+// Вызываем функцию подключения
+connectDB();
 
 app.listen(PORT, () => {
-	console.log('http://localhost:5000')
-})
+	console.log('http://localhost:5000');
+});
