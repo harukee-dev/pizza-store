c82be08106327c1b2f8e84d99a54279a83878bb2
harukee-dev <hdjemsmc@gmail.com>
2025-07-07 10:11:33 +0300

    added all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Header } from './Components/Header/Header'
 import { ErrorPage } from './Pages/ErrorPage/ErrorPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { MainPage } from './Pages/MainPage/MainPage'
+import { MenuPage } from './Pages/MenuPage/MenuPage'
+import { CartPage } from './Pages/CartPage/CartPage'
+import { OrdersPage } from './Pages/OrdersPage/OrdersPage'
+import { OrderPage } from './Pages/OrderPage/OrderPage'
+import { ProfilePage } from './Pages/ProfilePage/ProfilePage'
+import { LoginPage } from './Pages/LoginPage/LoginPage'
+import { RegisterPage } from './Pages/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <ErrorPage />
-    </div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/order" element={<OrderPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
