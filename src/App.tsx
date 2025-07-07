e6645e131f84b8d076c2c444fe6396acec91a00a
RogeYy <egorbazenov2008@gmail.com>
2025-07-07 10:40:22 +0300

    connect MongoDb to node js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Header } from './Components/Header/Header'
-import { Footer } from "./Components/Footer/Footer";
-import { ErrorPage } from './Pages/ErrorPage/ErrorPage'
+import { MainPage } from './Pages/MainPage/MainPage';
 
 function App() {
   return (
--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 
 const app = express();
@@ -8,12 +8,18 @@ const PORT = 5000;
 app.use(express.json());
 
 
-const DB_URL = 'mongodb+srv://Htabos:14563322asdZ@pizzadb.8u4bd1x.mongodb.net/?retryWrites=true&w=majority&appName=PizzaDB'; 
+const client = new MongoClient('mongodb+srv://Htabos:14563322asdZ@pizzadb.8u4bd1x.mongodb.net/pizza-menu?retryWrites=true&w=majority&appName=PizzaDB');
 
 async function connectDB() {
   try {
-    await mongoose.connect(DB_URL);
+    await client.connect();
     console.log('MongoDB подключена успешно!');
+    const allPizza = client.db().collection('pizza');
+    //await allPizza.insertOne({pizzaName: 'Peperoni', price: 800});
+    const pizza = await allPizza.findOne({pizzaName: 'Paprica'});
+    console.log(pizza);
+
+
   } catch (err) {
     console.error('Ошибка подключения к MongoDB:', err.message);
     process.exit(1); // Завершаем процесс при ошибке
